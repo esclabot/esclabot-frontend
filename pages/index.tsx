7748f66f202c84248a110a24c9a171aa099c137a
esclabot <davelogan69@hotmail.com>
2026-09-16 21:38:33 -0500

    Add main page component with welcome message

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <main style={{display:"flex",height:"100vh",alignItems:"center",justifyContent:"center"}}>
+      <h1>Bienvenido a ESCLABOT</h1>
+    </main>
+  )
+}
